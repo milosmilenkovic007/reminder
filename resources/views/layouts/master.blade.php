3327d39bff3da09458e301447aa913214a2381cd
milosmilenkovic007 <56878604+milosmilenkovic007@users.noreply.github.com>
2020-04-05 03:09:29 +0200

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,17 +3,17 @@
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
         <link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.14.30/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-        <title>Appointment reminders - @yield('title', 'reminders')</title>
+        <title>SMS Reminder System - @milos('title', 'reminders')</title>
     </head>
     <body>
         <nav class="navbar navbar-default">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">Appointment reminders</a>
+                <a class="navbar-brand" href="#">SMS Reminder</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ route('appointment.index') }}">Index</a></li>
-                    <li><a href="{{ route('appointment.create') }}">New appointment</a></li>
+                    <li><a href="{{ route('appointment.index') }}">Home</a></li>
+                    <li><a href="{{ route('appointment.create') }}">New Reminder</a></li>
                 </ul>
             </div>
         </nav>
@@ -21,8 +21,8 @@
             @yield('content')
         </div>
         <footer class="container">
-            Made with <i class="fa fa-heart"></i> by your pals
-            <a href="http://www.twilio.com">@twilio</a>
+            By Milos Milenkovic
+            <a href="https://milosmilenkovic.ml">@milos</a>
         </footer>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
